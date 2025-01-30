ba47cdb9171360d301078e60b972bfbc1838c3fc
Almos Mali <almos0002@gmail.com>
2025-01-30 22:18:28 +0545

    display routes fix

--- routes/web.php
+++ routes/web.php
@@ -4,20 +4,25 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\NxLeakController;
 use App\Http\Controllers\WallpaperController;
 use App\Http\Controllers\AccessTokenController;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
     return view('auth.login');
 });
 
-Auth::routes();
-
-Route::get('/dashboard', [App\Http\Controllers\HomeController::class, 'index'])->name('dashboard');
-
-
 // Token generation
 Route::post('/generate-token', [AccessTokenController::class, 'generate'])
      ->middleware('throttle:5,1')
      ->name('generate.token');
+
+// Display Routes Only
+Route::get('/n/{slug}', [NxLeakController::class, 'display'])->name('displaynx')->withoutMiddleware('auth');
+Route::get('/w/{slug}', [WallpaperController::class, 'display'])->name('displaywp')->withoutMiddleware('auth');
+
+// Authentication
+Auth::routes();
+
+Route::get('/dashboard', [App\Http\Controllers\HomeController::class, 'index'])->name('dashboard');
 
 // NxLeak Routes
 Route::get('/nxleak', [NxLeakController::class, 'add'])->name('addnx');
@@ -25,7 +30,6 @@ Route::post('/nxleak/create', [NxLeakController::class, 'create'])->name('create
 Route::get('/nxleak/fetch/{id}', [NxLeakController::class, 'fetch'])->name('fetchnx');
 Route::post('/nxleak/update', [NxLeakController::class, 'update'])->name('updatenx');
 Route::get('/nxleak/delete/{slug}', [NxLeakController::class, 'delete'])->name('deletenx');
-Route::get('/n/{slug}', [NxLeakController::class, 'display'])->name('displaynx');
 
 // Wallpapers Routes
 Route::get('/wallpaper', [WallpaperController::class, 'add'])->name('addwp');
@@ -33,4 +37,3 @@ Route::post('/wallpaper/create', [WallpaperController::class, 'create'])->name('
 Route::get('/wallpaper/fetch/{id}', [WallpaperController::class, 'fetch'])->name('fetchwp');
 Route::post('/wallpaper/update', [WallpaperController::class, 'update'])->name('updatewp');
 Route::get('/wallpaper/delete/{slug}', [WallpaperController::class, 'delete'])->name('deletewp');
-Route::get('/w/{slug}', [WallpaperController::class, 'display'])->name('displaywp');
